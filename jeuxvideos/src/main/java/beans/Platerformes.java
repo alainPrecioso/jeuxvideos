0bae5451f50365941f2dc176c6c07a30a74b7e1d
r.cilia <r.cilia@ICS-JAVA-2.pedago.ccinca.fr>
2022-07-26 12:17:41 +0200

    création des classes suivis de leur geter et seter :Classe Genre,Classe Genres,Classe Plateforme,Classe Plateformes

--- jeuxvideos/src/main/java/beans/Platerformes.java
+++ jeuxvideos/src/main/java/beans/Platerformes.java
@@ -8,5 +8,23 @@ public class Platerformes implements Serializable {
 	 * 
 	 */
 	private static final long serialVersionUID = 1145773800165425292L;
+	// Création des classes suivis de la méthode get et set 
+	private String Nom;
+	private String Description;
 
+	public String getNom() {
+		return Nom;
+	}
+
+	public void setNom(String nom) {
+		Nom = nom;
+	}
+
+	public String getDescription() {
+		return Description;
+	}
+
+	public void setDescription(String description) {
+		Description = description;
+	}
 }
