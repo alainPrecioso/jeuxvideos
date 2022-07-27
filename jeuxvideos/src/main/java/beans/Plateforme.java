0886a6f0647b1dff351a443d72e014c7b1b5672d
r.cilia <r.cilia@ICS-JAVA-2.pedago.ccinca.fr>
2022-07-27 08:51:26 +0200

    Accessibilité des propriétéés mis en private

--- jeuxvideos/src/main/java/beans/Plateforme.java
+++ jeuxvideos/src/main/java/beans/Plateforme.java
@@ -1,5 +1,24 @@
 package beans;
 
 public class Plateforme {
+	// Création des classes suivis de la méthode getters et setters.
+	// Accessibilité de préférence en private.
+	private String Nom;
+	private String Description;
+	
+	public String getNom() {
+		return Nom;
+	}
 
+	public void setNom(String nom) {
+		Nom = nom;
+	}
+
+	public String getDescription() {
+		return Description;
+	}
+
+	public void setDescription(String description) {
+		Description = description;
+	}
 }
